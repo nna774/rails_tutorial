1ae68583e074f763ebc5dcf777308f0dd7991e8a
NoNameA 774 <nonamea774@gmail.com>
2015-11-22 19:23:29 +0000

    WIP ~8.4.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # redirect to user page
       log_in user
+      remember user
       redirect_to user
     else
       # show error page
